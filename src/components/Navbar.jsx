94bf4e94d3c81ebf4ef44757f6b4386e58465d58
lwelling <53800195+lwelling@users.noreply.github.com>
2025-07-17 06:33:59 -0700

    Add navbar and configure SPA fallback

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white px-4 py-3 shadow-md">
+      <ul className="flex space-x-4">
+        <li>
+          <Link to="/" className="hover:underline">Home</Link>
+        </li>
+        <li>
+          <Link to="/brainrotaas" className="hover:underline">BrainROTAaS</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
