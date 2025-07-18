35c0422478130e3fa0827363a76fb55172d10801
lwelling <53800195+lwelling@users.noreply.github.com>
2025-07-18 16:32:10 -0700

    Add Javier page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,9 @@ export default function Navbar() {
         <li>
           <Link to="/gaslight" className="hover:underline">GaslightGPT</Link>
         </li>
+        <li>
+          <Link to="/javier" className="hover:underline">Javier</Link>
+        </li>
       </ul>
     </nav>
   );
